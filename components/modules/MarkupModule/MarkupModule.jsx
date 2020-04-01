5f6ea352eebc6f707dfd7d54e0b34b60bee82619
dgozen <55344620+dgozen@users.noreply.github.com>
2020-04-01 21:30:58 +0200

    chrome vs firefox make me want to kill my self

--- components/modules/MarkupModule/MarkupModule.jsx
+++ components/modules/MarkupModule/MarkupModule.jsx
@@ -2,7 +2,7 @@ import style from './MarkupModule.scss';
 
 const MarkupModule = ({ text, href }) => {
 	return (
-		<p>
+		<p className={style.root}>
 			{text}{' '}
 			<a target='_blank' href={href}>
 				{href}
